7f05b891548ffccc20cabbd8d6276e2c27d4024f
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-07 10:37:15 +0530

    Create local.tf

---
+++ day01/terraform-course/terraform-local/local.tf
@@ -0,0 +1,4 @@
+  resource "local_file" "devops" {
+          filename = "/home/ubuntu/terraform-course/terraform-local/devops_automated.txt
+          content = "This is the day-1 of terraweek challenge in which we will learn basics of it."
+  }
