f6d5a68bd75b3d90ca0e5a897286e0d2bd068534
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-10 23:08:24 +0530

    Create provider.tf

---
+++ day06/terraform-provider/provider.tf
@@ -0,0 +1,7 @@
+# Authentication using access key
+
+provider "aws" {
+        access_key = "AKIAZDXMNSUCC64NNYJQ"
+        secret_key = "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"
+        region = "us-east-1"
+}
