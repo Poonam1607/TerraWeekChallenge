f3507e885cf1715c831e06a00509edbd36e9bd68
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-11 18:50:12 +0530

    Update subnet-group.tf

--- day07/terraform-aws-vpc-proj/subnet-group.tf
+++ day07/terraform-aws-vpc-proj/subnet-group.tf
@@ -1,7 +1,7 @@
 #creating subnet group
 resource "aws_db_subnet_group" "My-Custom-subgroup" {
   name       = "my-custom-subgroup"
-  subnet_ids = [aws_subnet.private-subnet1.id, aws_subnet.private-subnet2.id]
+  subnet_ids = aws_subnet.private-subnets.id
   tags = {
     Name = "My data base subnet group"
   }
