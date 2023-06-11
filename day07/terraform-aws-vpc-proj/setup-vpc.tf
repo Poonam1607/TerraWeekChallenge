d6c7601d750ad909064073528fedf0a656dc36c9
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-11 18:28:52 +0530

    Update setup-vpc.tf

--- day07/terraform-aws-vpc-proj/setup-vpc.tf
+++ day07/terraform-aws-vpc-proj/setup-vpc.tf
@@ -1,10 +1,10 @@
 # Create AWS VPC in eu-central-1
 # CIDR - 10.0.0.0/16
-resource "aws_vpc" "vpc-jhooq-eu-central-1" {
+resource "aws_vpc" "custom-vpc" {
   cidr_block = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
-    Name = "VPC: jhooq-eu-central-1"
+    Name = "Customvpc"
   }
 }
 
