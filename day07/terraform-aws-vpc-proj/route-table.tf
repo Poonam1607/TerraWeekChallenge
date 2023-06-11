91a81f06012a5bf07140775dbb543a26f5a4a34a
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-11 19:08:43 +0530

    Update route-table.tf

--- day07/terraform-aws-vpc-proj/route-table.tf
+++ day07/terraform-aws-vpc-proj/route-table.tf
@@ -1,23 +1,23 @@
-resource "aws_route_table" "jhooq_public_route_table" {
-  vpc_id = aws_vpc.vpc-jhooq-eu-central-1.id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.custom_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.public_internet_gateway.id
+    gateway_id = aws_internet_gateway.custom_internet_gateway.id
   }
   tags = {
-    Name = "RT Public: For Jhooq EU Central Project "
+    Name = "public_route_table"
   }
 }
 
-resource "aws_route_table" "jhooq_private_route_table" {
+resource "aws_route_table" "private_route_table" {
   count      = length(var.cidr_private_subnet)
-  vpc_id = aws_vpc.vpc-jhooq-eu-central-1.id
-  depends_on = [aws_nat_gateway.nat_gateway]
+  vpc_id = aws_vpc.custom_vpc.id
+  depends_on = [aws_nat_gateway.custom_nat_gateway]
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
+    nat_gateway_id = aws_nat_gateway.custom_nat_gateway[count.index].id
   }
   tags = {
-    Name = "RT Private: For Jhooq EU Central Project "
+    Name = "private_route_table"
   }
 }
