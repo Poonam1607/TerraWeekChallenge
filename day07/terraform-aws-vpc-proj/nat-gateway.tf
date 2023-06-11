ed4ee5cc67d116d4cd04a3269d47649146337e43
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-11 18:37:37 +0530

    Update nat-gateway.tf

--- day07/terraform-aws-vpc-proj/nat-gateway.tf
+++ day07/terraform-aws-vpc-proj/nat-gateway.tf
@@ -1,12 +1,12 @@
-resource "aws_eip" "nat_eip" {
+resource "aws_eip" "custom-elastic-ip" {
   count = length(var.cidr_private_subnet)
   vpc = true
 }
 
-resource "aws_nat_gateway" "nat_gateway" {
+resource "aws_nat_gateway" "custom-nat-gateway" {
   count      = length(var.cidr_private_subnet)
   depends_on = [aws_eip.nat_eip]
-  allocation_id = aws_eip.nat_eip[count.index].id
+  allocation_id = aws_eip.custom-elastic.id
   subnet_id = aws_subnet.aws_jhooq_private_subnets[count.index].id
   tags = {
     "Name" = "Private NAT GW: For Jhooq EU Central Project "
