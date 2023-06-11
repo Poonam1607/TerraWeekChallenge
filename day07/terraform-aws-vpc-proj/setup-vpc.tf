069390e60af0f3e9690c8b8ff7bf552a8028f4b0
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-11 17:53:40 +0530

    Update setup-vpc.tf

--- day07/terraform-aws-vpc-proj/setup-vpc.tf
+++ day07/terraform-aws-vpc-proj/setup-vpc.tf
@@ -2,6 +2,7 @@
 # CIDR - 10.0.0.0/16
 resource "aws_vpc" "vpc-jhooq-eu-central-1" {
   cidr_block = var.vpc_cidr
+  instance_tenancy = "default"
   tags = {
     Name = "VPC: jhooq-eu-central-1"
   }
