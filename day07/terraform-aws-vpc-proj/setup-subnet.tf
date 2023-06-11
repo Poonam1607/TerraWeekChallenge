79245d6ca63c8960bac806fd91b53e0cd81d4abc
Poonam Pawar <80185070+Poonam1607@users.noreply.github.com>
2023-06-11 18:46:54 +0530

    Update setup-subnet.tf

--- day07/terraform-aws-vpc-proj/setup-subnet.tf
+++ day07/terraform-aws-vpc-proj/setup-subnet.tf
@@ -1,26 +1,26 @@
 # Setup public subnet
-resource "aws_subnet" "aws_jhooq_public_subnets" {
+resource "aws_subnet" "public-subnets" {
   count      = length(var.cidr_public_subnet)
-  vpc_id     = aws_vpc.vpc-jhooq-eu-central-1.id
+  vpc_id     = aws_vpc.custom-vpc.id
   cidr_block = element(var.cidr_public_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Subnet-Public : Public Subnet ${count.index + 1}"
+    Name = "My-public-subnet ${count.index + 1}"
   }
 }
 
 # Setup private subnet
-resource "aws_subnet" "aws_jhooq_private_subnets" {
+resource "aws_subnet" "private-subnets" {
   count      = length(var.cidr_private_subnet)
-  vpc_id     = aws_vpc.vpc-jhooq-eu-central-1.id
+  vpc_id     = aws_vpc.custom-vpc.id
   cidr_block = element(var.cidr_private_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "Subnet-Private : Private Subnet ${count.index + 1}"
+    Name = "My-private-subnet ${count.index + 1}"
   }
 }
 
